90e712c625382312b5c84f8f926d688d69d67ba1
AlejandroTorresR <alejandrotorresroa@gmail.com>
2019-12-27 21:24:48 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# brain-out
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/brain-out)
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  public fruits: any = ['strawberry', 'watermelon', 'apple', 'banana'];
+  public question: string = 'Encuentra al más grande';
+  public level: string = 'Lv. 1';
+  constructor(public alertController: AlertController) {}
+
+  clickFruit(fruit){
+    if(fruit === 'watermelon'){
+      console.log('success', fruit)
+    } else { console.log('error', fruit) }
+    this.presentAlert();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+}
