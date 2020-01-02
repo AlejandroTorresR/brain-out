b855ea7de7b637fb6f67cec098cbf7345aa882aa
AlejandroTorresR <alejandrotorresroa@gmail.com>
2020-01-02 17:56:18 -0600

    3 files modified array

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public levels: any = [1,2,3,4,5]
+  public levels: any = [
+    {name: 'lvl. 1', url: 'lvl001'},
+    {name: 'lvl. 2', url: 'lvl002'},
+    {name: 'lvl. 3', url: 'lvl003'},
+    {name: 'lvl. 4', url: 'lvl004'},
+    {name: 'lvl. 5', url: 'lvl005'}
+  ];
 
   constructor() {}
 
