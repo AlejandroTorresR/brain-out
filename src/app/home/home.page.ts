71f4b15508bf6118978af825549f3dbaf496901f
AlejandroTorresR <alejandrotorresroa@gmail.com>
2020-01-05 16:53:24 -0600

    2 files modified

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,14 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   public levels: any = [
-    {name: 'lv.1', url: '/lv001'},
-    {name: 'lv.2', url: '/lv002'},
-    {name: 'lv.3', url: '/lv003'},
-    {name: 'lv.4', url: '/lv004'},
-    {name: 'lv.5', url: '/lv005'}
+    {name: 'lv.1', url: '/lv001', solved: false},
+    {name: 'lv.2', url: '/lv002', solved: false},
+    {name: 'lv.3', url: '/lv003', solved: false},
+    {name: 'lv.4', url: '/lv004', solved: false},
+    {name: 'lv.5', url: '/lv005', solved: false}
   ];
   public progressLv: string = localStorage.getItem('progressLv');
 
-  constructor() {}
+  constructor() {
+    this.checkSolvedLevels();
+  }
 
-}
+  checkSolvedLevels(){
+    for(let i in this.levels){
+      if(localStorage.getItem(this.levels[i].url)){
+        this.levels[i].solved = true;
+      }
+    }
+  }
+
+}  
