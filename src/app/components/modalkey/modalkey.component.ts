5d8c5d5866b13baf2941ff6a7b91b425fc56ed1e
AlejandroTorresR <alejandrotorresroa@gmail.com>
2020-01-14 18:12:14 -0600

    4 files modified fixed lottie modalkey

--- src/app/components/modalkey/modalkey.component.ts
+++ src/app/components/modalkey/modalkey.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { GeneralService } from '../../providers/general.service';
 
 @Component({
 	selector: 'app-modalkey',
@@ -8,26 +9,36 @@ import { ModalController, NavParams } from '@ionic/angular';
 })
 export class modalkeyPage implements OnInit {
 
-  public interactiveKeys = {
-    loop: false,
-    prerender: true,
-    autoplay: true,
-    autoloadSegments: true,
-    path: 'assets/lottie/modalkey.json'
-  };
+  public interactiveKeys: Object;
+  public anim: any;
 
 	constructor(
+    public generalService: GeneralService,
 		public modalController:ModalController, 
 		private navParams: NavParams) {
-
+      this.interactiveKeys = {
+        loop: false,
+        renderer: 'svg',
+        autoplay: false,
+        autoloadSegments: false,
+        path: 'assets/lottie/modalkey.json'
+      };
 	}
 
 	ngOnInit() {
-
+    setTimeout(()=>{
+      this.anim.currentRawFrame = 1;
+      this.anim.play();
+    }, 0)
 	}
 
+  handleAnimation(anim: any) {
+        this.anim = anim;
+    }
+
 	close(){
-		this.modalController.dismiss()
+		this.modalController.dismiss();
+    this.generalService.pushKeys();
 	}
 
 
